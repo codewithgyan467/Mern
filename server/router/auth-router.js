e6c58533935c9418c71450de1063e71290d7d853
codewithgyan467 <duwarahgyandeep248@gmail.com>
2024-10-23 20:50:25 +0530

    controllers

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,6 +1,6 @@
 const express =require("express");
 const router = express.Router();
-const {home}=require("../controllers/auth-controller")
+const {home}=require("../controllers/auth-controller");
 
 router.route("/").get(home);
 router.route('/register').get(register)
--- server/server.js
+++ server/server.js
@@ -2,18 +2,15 @@ const express = require("express");
 const app = express();
 const home = require("../controllers/authcontrollers")
 app.use(express.json());
-// Correct path to the auth-router.js file
+
 const authRouter = require("./router/auth-router");
 
-// Use the auth router for /api/auth
 app.use("/api/auth", authRouter);
 
-// Default route (home page)
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to the homepage");
 });
 
-// Start the server
 const port = 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
